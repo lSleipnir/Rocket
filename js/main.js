453f49c5f222f25fbd2d5d0ca72df612fe7d4a49
Elodie Casciola <55943913+lSleipnir@users.noreply.github.com>
2020-03-31 09:48:40 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,11 +7,15 @@ var firingButton = document.getElementById('firing-button');
 var span = document.getElementsByTagName('span')[0];
 var count = 11;
 var img = document.getElementById('rocket');
-
+var star = document.getElementById('star');
 
 /***********************************************************************************/
 /* ********************************** FONCTIONS ************************************/
 /***********************************************************************************/
+
+function getRandomInteger (min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function myTimer() {
 	firingButton.classList.add("disabled");
@@ -32,8 +36,27 @@ function chronometre() {
     }, 1000);
 }
 
+
 /************************************************************************************/
 /* ******************************** CODE PRINCIPAL **********************************/
 /************************************************************************************/
 
 firingButton.addEventListener("click", myTimer);
+
+var classDiv;
+for (let i = 0; i < 150; i++) {
+	switch(getRandomInteger(1,3)) {
+		case 1:
+			classDiv = "tiny";
+			break;
+		case 1:
+			classDiv = "normal";
+			break;
+		default:
+			classDiv = "big";
+	}
+	var topStar = getRandomInteger(1,99);
+	var leftStar = getRandomInteger(1,99);
+	star.innerHTML += '<div class="star ' + classDiv + '" style=" top: ' + topStar + '%; left: ' + leftStar + '%;"></div>';
+}
+
